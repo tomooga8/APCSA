db5c4039851789ff79e50217ca3ed06a8fe6a358
tomooga8 <toga@hillers.org>
2021-10-26 09:22:57 -0400

    Boolean Practice

---
+++ booleanExample/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static boolean myTest(){
+        boolean test = 3 < 5;
+        if(test){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public static boolean otherTest(){
+        boolean test = 3 < 5;
+        return test;
+    }
+    public static void main(String[] args){
+        System.out.println(myTest());
+        System.out.println(otherTest());
+    }
+}
